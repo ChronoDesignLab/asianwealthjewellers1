6dd4419abb48c71bfd1f8f7ce9abcba4fdb502f1
Eric <emekaeric12@gmail.com>
2024-04-12 17:03:16 +0100

    last commit

--- components/Why/Changing.tsx
+++ components/Why/Changing.tsx
@@ -18,9 +18,10 @@ export default function Changing() {
 
   https: return (
     <div className="padding py-20">
-      <div className=" text-center text-2xl text-gray-800 lg:text-2xl xl:text-3xl mb-32 flex flex-col gap-3 font-semibold ">
-        <p className="">We're changing the way people think about the</p>
-        <p> secondary-luxury watch market.</p>
+      <div className=" text-center text-xl text-gray-800 lg:text-2xl xl:text-3xl mb-8 sm:mb-12 mb:mb-16 lg:mb-32 flex flex-col gap-3 font-semibold ">
+        <p className="hidden lg:block">We're changing the way people think about the</p>
+        <p className="hidden lg:block"> secondary-luxury watch market.</p>
+        <p className="lg:hidden">We're changing the way people think about the secondary-luxury watch market. </p>
       </div>
       <div className="flex flex-col lg:flex-row container3  gap-5">
         <div className="h-[600px] flex-1 w-full">
@@ -35,10 +36,10 @@ export default function Changing() {
             className="h-full w-full rounded object-cover"
           />
         </div>
-        <div className="lg:pl-14 flex-[1.75] flex flex-col gap-5 justify-evenly">
+        <div className="lg:pl-14 flex-[1.75] items-center lg:items-start text-center lg:text-start flex flex-col gap-5 justify-evenly">
           {data.map((item, key) => (
             <div className="">
-              <p className="text-[24px] font-semibold text-gray-800">
+              <p className="text-xl lg:text-[24px] font-semibold text-gray-800">
                 {item[0]}
               </p>
               <p className="mt-2 w-[90%] text-sm leading-loose">{item[1]}</p>
